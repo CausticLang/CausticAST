39bc208dd55f17625d4602b92fd53ad8a6a6b774
Shae <a.thomas.vc@gmail.com>
2024-04-23 12:21:15 -0700

    Implemented exception-related statements

--- src/caustic/cst/statement.py
+++ src/caustic/cst/statement.py
@@ -4,12 +4,14 @@
 
 #> Imports
 from dataclasses import dataclass, field
+from collections import abc as cabc
 
 from .bases import CausticASTNode, BaseStatement
 #</Imports
 
 #> Header >/
-__all__ = ('ImportStatement',)
+__all__ = ('ImportStatement',
+           'RaiseStatement', 'TryStatement', 'CatchStatement', 'LowerStatement')
 
 _dc = dataclass(slots=True)
 
@@ -19,3 +21,19 @@ class ImportStatement(BaseStatement):
     target: CausticASTNode
     i_from: CausticASTNode | None = field(default=None, kw_only=True)
     i_as:   CausticASTNode | None = field(default=None, kw_only=True)
+
+# Exception-related
+class RaiseStatement(BaseStatement):
+    '''Represents an exception-raising statement'''
+    target: CausticASTNode
+class TryStatement(BaseStatement):
+    '''Represents an exception-handling statement/block'''
+    block: CausticASTNode
+    catch: CatchStatement = field(kw_only=True)
+class CatchStatement(BaseStatement):
+    '''Represents the "catch" part of a `TryStatement`'''
+    catches: cabc.Sequence[CausticASTNode]
+class LowerStatement(BaseStatement):
+    '''Represents an exception-ignoring statement/block'''
+    lowers: cabc.Sequence[CausticASTNode]
+    block: CausticASTNode
