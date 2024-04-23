b1e21fd75801486d6dd4c97207bab779d154c18a
Shae <a.thomas.vc@gmail.com>
2024-04-23 12:59:29 -0700

    Implemented arithmetic and assignment binary operators

--- src/caustic/cst/expression.py
+++ src/caustic/cst/expression.py
@@ -3,11 +3,16 @@
 '''Expression nodes'''
 
 #> Imports
-from .bases import BaseUnaryOperator
+from .bases import BaseUnaryOperator, BaseBinaryOperator
 #</Imports
 
 #> Header >/
-__all__ = ('UPlus', 'UMinus', 'Increment', 'Decrement', 'BitInvert', 'LogNot')
+__all__ = (# Unary
+           'UPlus', 'UMinus', 'Increment', 'Decrement', 'BitInvert', 'LogNot',
+           # Binary
+           'AssignExpr',
+           ## Arithmetic
+           'Add', 'Sub', 'Mult', 'Div', 'Mod', 'MMul')
 
 # Unary
 class UPlus(BaseUnaryOperator):
@@ -27,4 +32,28 @@ class BitInvert(BaseUnaryOperator):
     __slots__ = ()
 class LogNot(BaseUnaryOperator):
     '''Represents a logical inversion operator'''
+    __slots__ = ()
+
+# Binary
+class AssignExpr(BaseBinaryOperator):
+    '''Represents an assignment-expression'''
+    __slots__ = ()
+## Arithmetic
+class Add(BaseBinaryOperator):
+    '''Represents an addition operator'''
+    __slots__ = ()
+class Sub(BaseBinaryOperator):
+    '''Represents a subtraction operator'''
+    __slots__ = ()
+class Mult(BaseBinaryOperator):
+    '''Represents a multiplication operator'''
+    __slots__ = ()
+class Div(BaseBinaryOperator):
+    '''Represents a division operator'''
+    __slots__ = ()
+class Mod(BaseBinaryOperator):
+    '''Represents a modulus operator'''
+    __slots__ = ()
+class MMul(BaseBinaryOperator):
+    '''Represents a matrix-multiplication operator'''
     __slots__ = ()
