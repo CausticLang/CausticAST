51a493f2dacd0f26552d4ea2582050f82704eb36
Shae <shae.m.vc@proton.me>
2024-04-29 12:33:40 -0700

    Implemented `Integer` literal atom

--- src/caustic/cst/expressions/atoms.py
+++ src/caustic/cst/expressions/atoms.py
@@ -2,14 +2,16 @@
 
 #> Imports
 import typing
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from collections import abc as cabc
 
 from . import Expression
 #</Imports
 
 #> Header >/
-__all__ = ('Atom', 'Identifier', 'DottedIdentifier')
+__all__ = ('Atom',
+           'Identifier', 'DottedIdentifier',
+           'Integer')
 
 _dc = dataclass(slots=True)
 
@@ -28,3 +30,11 @@ class Identifier(Atom):
 class DottedIdentifier(Atom):
     '''Represents a dotted identifier'''
     names: cabc.Sequence[str] | typing.Any
+
+# Literals
+## Numerics
+@_dc
+class Integer(Atom):
+    '''Represents an integer literal'''
+    val: int | str | typing.Any
+    base: int | typing.Any = field(default=10, kw_only=True)
