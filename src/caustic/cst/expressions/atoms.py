a48e64fdb66759680d08b7d53b77954df0b230a8
Shae <shae.m.vc@proton.me>
2024-04-29 12:01:42 -0700

    Implemented `Identifier` and `DottedIdentifier` nodes

--- src/caustic/cst/expressions/atoms.py
+++ src/caustic/cst/expressions/atoms.py
@@ -1,13 +1,15 @@
 #!/bin/python3
 
 #> Imports
+import typing
 from dataclasses import dataclass
+from collections import abc as cabc
 
 from . import Expression
 #</Imports
 
 #> Header >/
-__all__ = ('Atom',)
+__all__ = ('Atom', 'Identifier', 'DottedIdentifier')
 
 _dc = dataclass(slots=True)
 
@@ -15,3 +17,14 @@ _dc = dataclass(slots=True)
 @_dc
 class Atom(Expression):
     '''The base class for atom nodes'''
+
+# Identifiers
+@_dc
+class Identifier(Atom):
+    '''Represents a (non-dotted) identifier'''
+    name: str | typing.Any
+
+@_dc
+class DottedIdentifier(Atom):
+    '''Represents a dotted identifier'''
+    names: cabc.Sequence[str] | typing.Any
