e958a7aa63c87d9661cfea52e4e137658eb047d7
Shae <shae.m.vc@proton.me>
2024-04-30 10:37:04 -0700

    Implemented `Declaration` and `Assignment` statement nodes

--- src/caustic/cst/statements/__init__.py
+++ src/caustic/cst/statements/__init__.py
@@ -1,12 +1,17 @@
 #!/bin/python3
 
 #> Imports
+import typing
 from dataclasses import dataclass
+from collections import abc as cabc
 
 from .. import CSTNode
+from .. import typedecl
+from .. import expressions
 #</Imports
 
-__all__ = ('Statement',)
+__all__ = ('Statement',
+           'Declaration', 'Assignment')
 
 _dc = dataclass(slots=True, kw_only=True)
 
@@ -14,6 +19,20 @@ _dc = dataclass(slots=True, kw_only=True)
 @_dc
 class Statement(CSTNode):
     '''The base class for all statement nodes'''
+
+@_dc
+class Declaration(Statement):
+    '''Represents a typed name declaration, along with an optional value'''
+    mods: cabc.Sequence[str] | None | typing.Any = None
+    type: typedecl.Type | typing.Any
+    name: expressions.atoms.Identifier | typing.Any
+    val: expressions.Expression | None | typing.Any = None
+
+@_dc
+class Assignment(Statement):
+    '''Represents an assignment to a name as a statement'''
+    name: expressions.atoms.DottedIdentifier | typing.Any
+    val: expressions.Expression | typing.Any
 #</Header
 
 #> Package
