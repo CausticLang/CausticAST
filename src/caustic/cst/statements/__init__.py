4ff9881ece12450e06b9223ae669678ce5b44129
Shae <shae.m.vc@proton.me>
2024-05-01 08:54:54 -0700

    Implemented `ForOf` loop statement node

--- src/caustic/cst/statements/__init__.py
+++ src/caustic/cst/statements/__init__.py
@@ -13,7 +13,7 @@ from .. import expressions
 __all__ = ('Statement',
            'Declaration', 'Assignment',
            'If', 'Elif', 'Else',
-           'While', 'For', 'DoWhile',
+           'While', 'DoWhile', 'For', 'ForOf',
            'objects')
 
 _dc = dataclass(slots=True, kw_only=True)
@@ -59,17 +59,24 @@ class While(Statement):
     condition: expressions.Expression | typing.Any
     body: CSTNode | typing.Any
 @_dc
-class For(While):
-    '''Represents a 3-part for-loop'''
-    condition: expressions.Expression | None | typing.Any
-    init: expressions.Expression | None | typing.Any
-    foreach: expressions.Expression | None | typing.Any
-@_dc
 class DoWhile(While):
     '''
         Represents a while-loop that checks its condition
             after its first run
     '''
+@_dc
+class For(While):
+    '''Represents a 3-part for-loop'''
+    condition: expressions.Expression | None | typing.Any
+    init: expressions.Expression | Declaration | None | typing.Any
+    foreach: expressions.Expression | None | typing.Any
+@_dc
+class ForOf(Statement):
+    '''Represents a 1(ish)-part for-loop'''
+    name: Declaration | None | typing.Any
+    iname: Declaration | None | typing.Any
+    target: expressions.Expression | typing.Any
+    body: CSTNode | typing.Any
 #</Header
 
 #> Package
