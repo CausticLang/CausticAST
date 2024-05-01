478a1a182fde75e7674a7b5851c71628349cd280
Shae <shae.m.vc@proton.me>
2024-05-01 09:25:46 -0700

    Implemented `(de)serialize[_to|_from]()` functions

--- src/caustic/cst/serialize.py
+++ src/caustic/cst/serialize.py
@@ -6,14 +6,17 @@
 '''
 
 #> Imports
+import io
 import pickle
 import typing
+import pickletools
 
 from . import CSTNode
 #</Imports
 
 #> Header >/
-__all__ = ('CSTPickler', 'CSTUnpickler')
+__all__ = ('CSTPickler', 'CSTUnpickler',
+           'serialize', 'deserialize', 'serialize_to', 'deserialize_from')
 
 class CSTPickler(pickle.Pickler):
     __slots__ = ()
@@ -29,3 +32,21 @@ class CSTUnpickler(pickle.Unpickler):
 
     def persistent_load(self, id: str) -> type[CSTNode]:
         return CSTNode.NODE_DIRECTORY[id]
+
+
+def serialize(cst: CSTNode, *, optimize: bool = True) -> bytes:
+    '''Serializes a CST to bytes'''
+    with io.BytesIO() as stream:
+        serialize_to(cst, stream)
+        return (pickletools.optimize(stream.getvalue())
+                if optimize else stream.getvalue())
+def serialize_to(cst: CSTNode, stream: typing.BinaryIO) -> None:
+    '''Serializes a CST to bytes into a stream'''
+    CSTPickler(stream).dump(cst)
+def deserialize(data: bytes) -> CSTNode:
+    '''Deserializes a previously serialized CST'''
+    with io.BytesIO(data) as stream:
+        return deserialize_from(stream)
+def deserialize_from(stream: typing.BinaryIO) -> CSTNode:
+    '''Deserializes a previously serialized CST from a stream'''
+    return CSTUnpickler(stream).load()
